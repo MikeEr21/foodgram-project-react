09cbbe496071081be30725fd4afae1cd71f959d3
MikeEr21 <77528007+MikeEr21@users.noreply.github.com>
2023-04-30 18:59:42 +0300

    Same deletions

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -1,21 +1,13 @@
 import os
-from datetime import timedelta
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'pcfz=mexzdz-$k&h)@3ls1vx83ybc=k-7!*w+lxjoku+2aou01'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
