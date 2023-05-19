c210249b4f58788e10cf5ea4128139a25a220f8a
MikeEr21 <77528007+MikeEr21@users.noreply.github.com>
2023-05-19 13:25:44 +0300

    Deploy

--- backend/api/v1/urls.py
+++ backend/api/v1/urls.py
@@ -8,35 +8,35 @@ from users.views import AuthToken, UsersViewSet, set_password
 
 app_name = 'api'
 
-router = DefaultRouter()
-router.register('v1/users', UsersViewSet)
-router.register('v1/tags', TagsViewSet)
-router.register('v1/ingredients', IngredientsViewSet)
-router.register('v1/recipes', RecipesViewSet)
+router_v1 = DefaultRouter()
+router_v1.register('users', UsersViewSet)
+router_v1.register('tags', TagsViewSet)
+router_v1.register('ingredients', IngredientsViewSet)
+router_v1.register('recipes', RecipesViewSet)
 
 
 urlpatterns = [
      path(
-          'v1/auth/token/login/',
+          'auth/token/login/',
           AuthToken.as_view(),
           name='login'),
      path(
-          'v1/users/set_password/',
+          'users/set_password/',
           set_password,
           name='set_password'),
      path(
-          'v1/users/<int:user_id>/subscribe/',
+          'users/<int:user_id>/subscribe/',
           AddAndDeleteSubscribe.as_view(),
           name='subscribe'),
      path(
-          'v1/recipes/<int:recipe_id>/favorite/',
+          'recipes/<int:recipe_id>/favorite/',
           AddDeleteFavoriteRecipe.as_view(),
           name='favorite_recipe'),
      path(
-          'v1/recipes/<int:recipe_id>/shopping_cart/',
+          'recipes/<int:recipe_id>/shopping_cart/',
           AddDeleteShoppingCart.as_view(),
           name='shopping_cart'),
-     path('', include(router.urls)),
+     path('', include(router_v1.urls)),
      path('', include('djoser.urls')),
-     path('v1/auth/', include('djoser.urls.authtoken')),
+     path('auth/', include('djoser.urls.authtoken')),
 ]
