595a4c0527b090868cf96d3dbafcbd756fdce827
MikeEr21 <77528007+MikeEr21@users.noreply.github.com>
2023-05-26 01:37:03 +0300

    Deploy

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -75,6 +75,7 @@ class UserListSerializer(
         fields = (
             'email',
             'username',
+            'id',
             'first_name',
             'last_name',
             'is_subscribed'
